8f83e89918224f7426356e71c8b7dbb6bd2d5838
Otto Dodoo <165803037+ottododoo@users.noreply.github.com>
2024-07-04 16:44:35 +0200

    Update UniBuddy.py

--- UniBuddy.py
+++ UniBuddy.py
@@ -1,8 +1,3 @@
-pip install -qqq -U langchain-huggingface
-pip install -qqq -U langchain
-pip install -qqq -U langchain-community
-pip install -qqq -U faiss-cpu
-
 from langchain_huggingface import HuggingFaceEndpoint, HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
