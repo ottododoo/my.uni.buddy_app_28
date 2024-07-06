5fae87060f43b1088ea8e9280111b1726136452b
Otto Dodoo <165803037+ottododoo@users.noreply.github.com>
2024-07-06 21:38:11 +0200

    Update UniBuddy.py

--- UniBuddy.py
+++ UniBuddy.py
@@ -1,21 +1,15 @@
-import os
-from dotenv import load_dotenv
-import streamlit as st
 from langchain_huggingface import HuggingFaceEndpoint, HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
+import streamlit as st
 
-# Load environment variables
-load_dotenv()
-hf_api_key = os.getenv('SECRET_NAME')
-
-# Initialize Hugging Face endpoint with token in model_kwargs
+# Hugging Face model details
 hf_model = "mistralai/Mistral-7B-Instruct-v0.3"
 llm = HuggingFaceEndpoint(repo_id=hf_model)
 
-# Initialize embeddings model
+# Embedding model details
 embedding_model = "sentence-transformers/all-MiniLM-L6-v2"
 embeddings_folder = "docs"
 embeddings = HuggingFaceEmbeddings(model_name=embedding_model, cache_folder=embeddings_folder)
@@ -24,10 +18,10 @@ embeddings = HuggingFaceEmbeddings(model_name=embedding_model, cache_folder=embe
 vector_db = FAISS.load_local("docs", embeddings, allow_dangerous_deserialization=True)
 retriever = vector_db.as_retriever(search_kwargs={"k": 3})
 
-# Initialize conversation memory
+# Conversation memory
 memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True, output_key='answer')
 
-# Define prompt template
+# Prompt template
 template = """You are a nice chatbot having a conversation with a human. Answer the question based only on the following context and previous conversation. Keep your answers short and succinct.
 
 Previous conversation:
@@ -38,26 +32,28 @@ Context to answer question:
 
 New human question: {question}
 Response:"""
-prompt_template = PromptTemplate(template=template, input_variables=["context", "question"])
+prompt = PromptTemplate(template=template, input_variables=["context", "question"])
 
-# Initialize Conversational Retrieval Chain
+# Conversational retrieval chain
 chain = ConversationalRetrievalChain.from_llm(llm, retriever=retriever, memory=memory,
                                               return_source_documents=True,
-                                              combine_docs_chain_kwargs={"prompt": prompt_template})
+                                              combine_docs_chain_kwargs={"prompt": prompt})
 
-# Streamlit App
-st.title("My.Unibuddy_Germany: Your Guide to Studying and Living in Germany")
+##### Streamlit App #####
+
+st.title("My.Unibuddy_Germany: Your Guide to Studying and living in Germany")
 
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.image("Design ohne Titel (2).png")
+   st.image("Design ohne Titel (2).png")
 
 with col2:
-    st.image("Design ohne Titel (1).png")
+   st.image("Design ohne Titel (1).png")
 
 with col3:
-    st.image("Design ohne Titel.png")
+   st.image("Design ohne Titel.png")
+    
 
 st.sidebar.title("Popular Questions")
 st.sidebar.markdown("""
@@ -83,36 +79,26 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 # React to user input
-if user_input := st.chat_input("How may I help you?"):
+if prompt := st.chat_input("How may I help you?"):
 
     # Display user message in chat message container
-    st.chat_message("user").markdown(user_input)
+    st.chat_message("user").markdown(prompt)
 
     # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": user_input})
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
-    # Begin spinner before answering question
+    # Begin spinner before answering question so it's there for the duration
     with st.spinner("Uncovering knowledge about studying in Germany..."):
 
-        try:
-            # Send question to chain to get answer
-            answer = chain(user_input)
+        # Send question to chain to get answer
+        answer = chain(prompt)
 
-            # Extract answer from dictionary returned by chain
-            response = answer["answer"]
+        # Extract answer from dictionary returned by chain
+        response = answer["answer"]
 
-            # Display chatbot response in chat message container
-            with st.chat_message("assistant"):
-                st.markdown(response)
+        # Display chatbot response in chat message container
+        with st.chat_message("assistant"):
+            st.markdown(response)
 
-            # Add assistant response to chat history
-            st.session_state.messages.append({"role": "assistant", "content": response})
-
-        except Exception as e:
-            # Handle and log errors
-            st.error(f"An error occurred: {e}")
-            if hasattr(e, 'response'):
-                st.error(f"Response: {e.response}")
-
-
-
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
